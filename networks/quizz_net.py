044da33c488c495b2da739e91fce9399cef348e1
Himanshu <047himanshu@gmail.com>
2020-05-30 23:06:37 +0530

    architecture

--- networks/quizz_net.py
+++ networks/quizz_net.py
@@ -7,6 +7,12 @@ class QuizDNN(nn.Module):
     def __init__(self):
         super(QuizDNN, self).__init__()
 
+        self.x0_block = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.2),
+        )
         self.x1_block = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
@@ -66,7 +72,7 @@ class QuizDNN(nn.Module):
         )
 
     def forward(self, x):
-        x1 = x
+        x1 = self.x0_blocl(x)
         x2 = self.x1_block(x1)
         x3 = self.x2_block(x1+x2)
 
