7bbd26120c04e7f9e8bddf09e732dad7028bbd71
Himanshu <047himanshu@gmail.com>
2020-05-30 16:37:28 +0530

    albumentation added

--- infdata/dataset/cifar10_data.py
+++ infdata/dataset/cifar10_data.py
@@ -3,12 +3,12 @@ Define custom dataset function or download pre-defined data from pytorch
 """
 
 from torchvision import datasets
-from infdata.transformation.cifar10_tf import Transforms
+from infdata.transformation.cifar10_tf import Transforms, AlbumTransforms
 import os
 
 curr_dir = os.path.dirname(__file__)
 
-class DownloadData(object):
+class DownloadDataOld(object):
     def __init__(self):
         self.data_path = os.path.join(curr_dir,"../../","data")
 
@@ -21,5 +21,21 @@ class DownloadData(object):
 
         self.cifar10_testdata = datasets.ImageFolder(root=os.path.join(self.data_path, "test-images"), 
                                                 transform=transf.test_transforms)
+
+        self.testdata_classes = self.cifar10_testdata.class_to_idx
+
+class DownloadData(object):
+    def __init__(self):
+        self.data_path = os.path.join(curr_dir,"../../","data")
+
+        transf = AlbumTransforms()
+        self.cifar10_traindata = datasets.CIFAR10(self.data_path, train=True, download=True,
+                                                transform=transf.get_train_transforms())
+        
+        self.cifar10_validdata = datasets.CIFAR10(self.data_path, train=False, download=True,
+                                                transform=transf.get_valid_transforms())
+
+        self.cifar10_testdata = datasets.ImageFolder(root=os.path.join(self.data_path, "test-images"), 
+                                                transform=transf.get_test_transforms())
 
         self.testdata_classes = self.cifar10_testdata.class_to_idx
--- infdata/transformation/cifar10_tf.py
+++ infdata/transformation/cifar10_tf.py
@@ -1,4 +1,17 @@
 from torchvision import transforms
+import cv2
+from albumentations import (
+	Compose,
+    HorizontalFlip,
+    Normalize,
+    Resize,
+    CoarseDropout,
+    Rotate,
+    GaussianBlur,
+    HueSaturationValue
+)
+
+from albumentations.pytorch import ToTensor
 
 class Transforms(object):
     def __init__(self):
@@ -20,3 +33,45 @@ class Transforms(object):
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
         ])
+
+class AlbumTransforms(object):
+    def __init__(self):
+        self.mean = (0.4914, 0.4822, 0.4465)
+        self.std = (0.2023, 0.1994, 0.2010)
+
+        self.transforms_elist = [ 
+            HueSaturationValue(p=0.25),
+			HorizontalFlip(p=0.5),
+			Rotate(limit=15),
+			CoarseDropout(max_holes=1, max_height=16, max_width=16, min_height=4,
+						    min_width=4, fill_value=mean*255.0, p=0.75)
+        ]
+
+        self.transforms_test = [
+            Resize(32,32)
+        ]
+
+        self.transforms_main = [
+            Normalize(mean=self.mean, std=self.std, max_pixel_value=255.0, p=1.0),
+		    ToTensor()
+        ]
+
+
+    def get_train_transforms(self):
+        train_transforms = Compose(self.transforms_elist.extend(self.transforms_main))
+        return lambda img:train_transforms(image=np.array(img))["image"]
+
+    def get_valid_transforms(self):
+        valid_transforms = Compose(self.transforms_main)
+        return lambda img:valid_transforms(image=np.array(img))["image"]
+
+    def get_test_transforms(self):
+        test_transforms = Compose(self.transforms_test.extend(self.transforms_main))
+        return lambda img:test_transforms(image=np.array(img))["image"]
+
+
+
+
+
+
+
