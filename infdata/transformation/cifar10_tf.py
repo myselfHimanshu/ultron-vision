b6146435f9da072569be22e7e9979371cbcb9b5e
Himanshu <047himanshu@gmail.com>
2020-06-04 22:14:47 +0530

    transformation changes

--- infdata/transformation/cifar10_tf.py
+++ infdata/transformation/cifar10_tf.py
@@ -42,10 +42,10 @@ class AlbumTransforms(object):
 
         self.transforms_elist = [ 
             RandomResizedCrop(height=32, width=32),
-            HorizontalFlip(),
+            HorizontalFlip(1.0),
             # RandomBrightnessContrast(),
 			Rotate(limit=7),
-			Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=self.mean*255.0, p=0.75),
+			Cutout(num_holes=1, max_h_size=16, max_w_size=16, fill_value=self.mean*255.0),
         ]
 
         self.transforms_test = [
