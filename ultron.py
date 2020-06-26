9878c00c6367b23f4feb0f0a3e5cb206963b9d2e
Himanshu <047himanshu@gmail.com>
2020-06-26 21:44:04 +0530

    changes

--- ultron.py
+++ ultron.py
@@ -21,22 +21,22 @@ def main():
     agent = agent_class(config)
 
     # visualize training set
-    # agent.visualize_set()
+    agent.visualize_set()
     
     # # train model
     agent.run()
-    # agent.finalize()
+    agent.finalize()
     
-    # # Create inference agent and pass the configuration to it and interpret
-    # iagent_class = globals()[config["inference_agent"]]
-    # iagent = iagent_class(config)
+    # Create inference agent and pass the configuration to it and interpret
+    iagent_class = globals()[config["inference_agent"]]
+    iagent = iagent_class(config)
     
-    # # plot learning rate, accuracy, loss graphs and misclassified images with gradcam
-    # iagent.show_per_class_accuracy()
-    # iagent.plot_lr_graph()
-    # iagent.plot_accuracy_graph()
-    # iagent.plot_loss_graph()
-    # iagent.show_misclassified_images()
+    # plot learning rate, accuracy, loss graphs and misclassified images with gradcam
+    iagent.show_per_class_accuracy()
+    iagent.plot_lr_graph()
+    iagent.plot_accuracy_graph()
+    iagent.plot_loss_graph()
+    iagent.show_misclassified_images()
 
 if __name__=="__main__":
     main()
